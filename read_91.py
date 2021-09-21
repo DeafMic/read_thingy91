aecab4d0b477f76553efdb9e8f565943b02e3e24
Mike <myke.dmytrenko@gmail.com>
2021-09-21 15:15:47 +0200

    Multiple
    Supposed to work and stream multiple thungy91 devices

--- read_91.py
+++ read_91.py
@@ -9,16 +9,53 @@ id=0
 pub = rospy.Publisher('sensor_data', std_msgs.msg.String, queue_size=10)
 rospy.init_node('sensor_pub', anonymous=True)
 to_publish=''
-
+messages=[]
+output={}
+devices=[]
+present=False
 try:
     while not rospy.is_shutdown():
         to_print=''
         response = requests.get('https://api.nrfcloud.com/v1/messages', headers=headers)
-        time.sleep(0.5)
+        
+        time.sleep(1)
+        
+        
+        
+        
+        
+        for eachItem in response.json()['items']:
+            
+            # output[each['deviceId']]= each['message']['appId']+' '+each['message']['data']
+            for each in devices:
+                if eachItem['deviceId']==each:
+                    present=True
+
+            if present==False:
+                devices.append(eachItem['deviceId'])
+        
+        for eachDev in devices:
+            message=[]
+            for eachItem in response.json()['items']:
+                if eachDev==eachItem['deviceId']:
+                    message.append(eachItem['message']['appId']+' '+eachItem['message']['data'])
+                
+            output[eachDev]=message
+
+
+
         id=id+1
         if id==len(response.json()['items'])-1:
             id=0
 
+
+        
+        
+   
+        
+        
+        
+        
         to_publish=response.json()['items'][id]['message']['appId']+' '+response.json()['items'][id]['message']['data']
         
         # print(to_publish)
@@ -30,6 +67,7 @@ try:
                 to_print+=','
             # print(response.json()['items'][i]['message']['appId'])
         print(to_print)
+        print('Output is ',output)
         pub.publish(to_print)
 except KeyboardInterrupt:
     print('interrupted!')
